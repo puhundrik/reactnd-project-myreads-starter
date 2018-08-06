a67852560c3779ea9914e56f28058286b6b696cc
Yuriy Petrushkin <ynn@athn.ru>
2018-08-06 13:59:32 +0300

    feat: added routing between pages

--- src/MyReads.js
+++ src/MyReads.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom'
 
 class MyReads extends Component {
     render() {
@@ -22,7 +23,7 @@ class MyReads extends Component {
                     </div>
                 </div>
                 <div className="open-search">
-                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    <Link to='/search'>Add a book</Link>
                 </div>
             </div>
         )
