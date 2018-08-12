375adf95b1a27e10878737dd956a28aad9bef682
Yuriy Petrushkin <ynn@athn.ru>
2018-08-12 16:52:07 +0300

    feat: Display message if there are no books on the shelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -3,21 +3,28 @@ import BookPreview from './BookPreview'
 
 class BookShelf extends Component {
     render() {
+        let booShelf;
+        if (this.props.books.length > 0) {
+            booShelf = <div className="bookshelf-books">
+                <ol className="books-grid">
+                {this.props.books.map((book) => (
+                    <li key={book.id}>
+                        <BookPreview
+                            book={book}
+                            onUpdateShelf = {this.props.onUpdateShelf}
+                        />
+                    </li>
+                ))}
+                </ol>
+            </div>;
+        } else {
+            booShelf = 'There are no books on the shelf';
+        }
+        
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {this.props.books.map((book) => (
-                        <li key={book.id}>
-                            <BookPreview
-                                book={book}
-                                onUpdateShelf = {this.props.onUpdateShelf}
-                            />
-                        </li>
-                    ))}
-                    </ol>
-                </div>
+                {booShelf}
             </div>
         )
     }
